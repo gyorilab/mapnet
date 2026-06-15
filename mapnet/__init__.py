6db65bfe808c9ab7847e771b4c00721dc440d080
Ben Gyori <ben.gyori@gmail.com>
2026-06-15 16:29:46 -0400

    Set version number

--- mapnet/__init__.py
+++ mapnet/__init__.py
@@ -1,5 +1,7 @@
 import logging
 
+__version__ = '0.1.0'
+
 logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
                             ' - %(message)s'),
                     level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
